5d982f5ca7c1d995b920359f05e4863cd57a5d3f
shreshth <shreshthsrivastava2@gmail.com>
2022-07-25 14:49:58 -0700

    [server] Fix CORS issue.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
 const PORT = 5000;
+
+app.use(cors());
 
 app.get('/players', (req, res) => {
     const players = [
